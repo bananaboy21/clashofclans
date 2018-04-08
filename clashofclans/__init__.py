dfe2bfd673c7be148a2ddd17ca262c65a609d85f
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-04-07 21:14:02 -0400

    add two endpoints: get profile and get clan

--- clashofclans/__init__.py
+++ clashofclans/__init__.py
@@ -1,1 +1,77 @@
+import aiohttp
+from box import Box
+import urllib.parse
 
+
+class CocError(Exception):
+    '''
+    The exception raised when the Clash of Clans API returns a status code
+    other than 200 (success). You will have to handle this error yourself.
+    '''
+    pass
+
+
+
+class Client:
+    '''
+    An object that represents the main Client of the clashofclans wrapper.
+    Through this object, you can request from the endpoints here.
+    '''
+
+    def __init__(self, token, session=None):
+        '''
+        Constructs the Client used for requests. 
+
+        ---Params---
+        token (str): The API key received from the Clash of Clans API website.
+        session (Optional, ClientSession): A ClientSession for this Client to use when making requests.
+        Defaults to an aiohttp.ClientSession().
+        '''
+        self.token = token
+        self.session = aiohttp.ClientSession() if session is None else session
+        self.headers = {'Authorization': self.token}
+        self.baseurl = "https://api.clashofclans.com/v1/"
+
+
+    async def _get(self, endpoint, query):
+        '''
+        This function requests to the API and returns the content that responds from it.
+        It is called in each of the endpoints.
+
+        You should never call this function directly. Instead, use a specified endpoint.
+        '''
+        query = urllib.parse.quote(str(query))
+        async with self.session.get("{}{}{}".format(self.baseurl, endpoint, query)) as resp:
+            if resp.status != 200:
+                raise CocError("An error occurred with Clash of Clans API. Code: {}".format(resp.status))
+            resp = await resp.json()
+            return Box(resp)
+
+
+    async def get_profile(self, query):
+        '''
+        Gets a COC profile of a player by the player tag.
+
+        ---Params---
+        query (str): The player's profile tag.
+        Note that removing the '#' is optional, this wrapper does it for you.
+
+        This requests to the /players/{playerTag} endpoint of the API.
+        '''
+        return await self._get("players/", query.strip("#"))
+
+
+    async def get_clan(self, query):
+        '''
+        Gets information for a COC clan by the clan tag.CocError
+
+        ---Params---
+        query (str): The clans's clan tag.
+        Note that removing the '#' is optional, this wrapper does it for you.
+
+        This requests to the /clans/{clanTag} endpoint of the API.
+        '''
+        return await self._get("clans/", query.strip("#"))
+
+
+    
