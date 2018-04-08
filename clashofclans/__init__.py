249df8fa7cb02488892182f36743bc5641449935
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-04-08 18:33:23 -0400

    add some endpoints: get_clan_from_profile, get_current_war, get_clan_members

--- clashofclans/__init__.py
+++ clashofclans/__init__.py
@@ -11,6 +11,16 @@ class CocError(Exception):
     pass
 
 
+class NotInWar(CocError):
+    '''
+    The exception raised when a request is made with get_current_war
+    but the clan was not currently in a war.
+
+    You will have to handle this error yourself.
+    '''
+    pass
+
+
 
 class Client:
     '''
@@ -41,7 +51,7 @@ class Client:
         You should never call this function directly. Instead, use a specified endpoint.
         '''
         query = urllib.parse.quote(str(query))
-        async with self.session.get("{}{}%23{}".format(self.baseurl, endpoint, query), headers=self.headers) as resp:
+        async with self.session.get("{}{}{}".format(self.baseurl, endpoint, query), headers=self.headers) as resp:
             if resp.status != 200:
                 errors = {
                     400: "Client provided incorrect parameters for the request.",
@@ -66,12 +76,12 @@ class Client:
 
         This requests to the /players/{playerTag} endpoint of the API.
         '''
-        return await self._get("players/", query.strip("#"))
+        return await self._get("players/%23", query.strip("#"))
 
 
     async def get_clan(self, query):
         '''
-        Gets information for a COC clan by the clan tag.CocError
+        Gets information for a COC clan by the clan tag.
 
         ---Params---
         query (str): The clans's clan tag.
@@ -79,7 +89,57 @@ class Client:
 
         This requests to the /clans/{clanTag} endpoint of the API.
         '''
-        return await self._get("clans/", query.strip("#"))
+        return await self._get("clans/%23", query.strip("#"))
 
+
+    async def get_clan_from_profile(self, query):
+        '''
+        A shortcut used to get information for a COC clan
+        through a player's tag. This will return information 
+        in the clan that they are currently in.
+
+        ---Params---
+        query (str): The players's player tag.
+        Note that removing the '#' is optional, this wrapper does it for you. 
+
+        This requests to:
+        1) The /players/{playerTag} endpoint for the clan tag.
+        2) The /clans/{clanTag} endpoint for clan information.
+        '''
+        profile = await self._get("players/%23", query.strip("#"))
+        clantag = profile.clan.tag
+        return await self._get("clans/%23", clantag.strip("#"))
+
+
+    async def get_current_war(self, query):
+        '''
+        Gets information for a clan's current war.
+
+        ---Params---
+        query (str): The clans's clan tag.
+        Note that removing the '#' is optional, this wrapper does it for you.
+
+        This requests to the /clans/{clanTag}/currentwar endpoint of the API.
+        '''
+        data = await self._get("clans/%23", "{}{}".format(query.strip("#"), "/currentwar"))
+        try:
+            data.teamSize
+        except AttributeError:
+            raise NotInWar("The clan is currently not in a war.")
+        return data
+
+
+    async def get_clan_members(self, query):
+        '''
+        Gets a list of members in a COC clan, from the clan tag.
+
+        ---Params---
+        query (str): The clans's clan tag.
+        Note that removing the '#' is optional, this wrapper does it for you.
+
+        This requests to the /clans/{clanTag}/members endpoint of the API.
+        '''
+        return await self._get("clans/%23", "{}{}".format(query.strip("#"), "/members"))
+        
 
     
